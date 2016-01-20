e82d8f367a0feffe99f7c8f415361d51aa97395a
Étienne Charignon <etienne.charignon@gmail.com>
2016-01-20 19:25:54 +0100

    commit initial
    problème en cours :
      - répétition s'incrémente sur chaque appuie de touche pendant la phase
      de controle
      - Controle inclu l'accumulation des touches et ne devrait pas.
      - le click n'est efficace que s'il est fait sur l'ardoise
      - Amméliorer le tirage des opérations

---
+++ 6x7=42.js
@@ -0,0 +1,62 @@
+var repetition = 0;
+var table = 7;
+var multiplicateur = 1;
+
+function operationSuivante() {
+  multiplicateur = Math.ceil(Math.random()*10);
+}
+
+function ecritOperationSur(ardoise, multiplicateur) {
+  ardoise.textContent= table + "x" + multiplicateur + "=";
+}
+
+function ecritResultatSur(ardoise, resultat){
+  ardoise.textContent=  ardoise.textContent + resultat;
+}
+
+function deplace(ardoise) {
+  if(repetition > 10) {
+    ardoise.style.top = Math.random() * 80 + "%";
+  }
+}
+
+function boucle(e) {
+  repetition += 1;
+  ecritOperationSur(ardoise, multiplicateur);
+  if(repetition < 20) {
+    window.onclick=boucle;
+    window.onkeypress=undefined;
+    apprendre(document.getElementById('ardoise'));
+  }
+  else {
+    window.onclick=undefined;
+    window.onkeypress=boucle;
+    if(e.keyCode)
+      controle(e);
+  }
+  document.getElementById('repetition').textContent = repetition;
+}
+
+function apprendre(ardoise) {
+  ecritResultatSur(ardoise, table * multiplicateur);
+  deplace(ardoise);
+  operationSuivante();
+}
+
+var proposition = "";
+
+function controle(e) {
+  var ardoise = document.getElementById('ardoise');
+  if(e.keyCode == 13) {
+    if(proposition == multiplicateur * table) {
+      operationSuivante();
+      ecritOperationSur(ardoise, multiplicateur);
+    }
+    else {
+    }
+    proposition="";
+    return;
+  }
+  proposition += e.keyCode - 48;
+  ecritResultatSur(ardoise, proposition);
+}
