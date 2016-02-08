a59c00a4b4f2e732f1886beaf739c2924b171a7a
Étienne Charignon <etienne.charignon@gmail.com>
2016-02-08 23:46:15 +0100

    Externalise la gestion du clavier pour les tests

---
+++ clavier.js
@@ -0,0 +1,20 @@
+function installeClavier(callback) {
+  $(document).on('keypress', function(e) {
+    var keyNumberMap = {
+      224:0, 48: 0,
+      38: 1, 49: 1,
+      233:2, 50: 2,
+      34: 3, 51: 3,
+      39: 4, 52: 4,
+      40: 5, 53: 5,
+      167:6, 54: 6,
+      232:7, 55: 7,
+      33: 8, 56: 8,
+      231:9, 57: 9
+    };
+
+    if(e && e.keyCode && e.keyCode in keyNumberMap) {
+      callback(keyNumberMap[e.keyCode]);
+    }
+  });
+}
